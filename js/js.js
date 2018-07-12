2e703d97faeb292307dec296d265e302bddbc419
Nguyen Van Khoa 20173196 <khoa.nv173196@sis.hust.edu.vn>
2018-07-12 14:41:56 +0700

    upload

---
+++ js/js.js
@@ -0,0 +1,138 @@
+$(document).ready(function() {
+	TweenMax.staggerFrom($('.nhieunut button'),0.5,{left:100, opacity:0},0.1)
+	$('.nut0').click(function(event) {
+		$('.noidung').addClass('noidunghienra0');
+		$('.momo').addClass('momohienra');
+	});
+	$('.nut1').click(function(event) {
+		$('.noidung').addClass('noidunghienra1');
+		$('.momo').addClass('momohienra');
+	});
+	$('.nut2').click(function(event) {
+		$('.noidung').addClass('noidunghienra2');
+		$('.momo').addClass('momohienra');
+	});
+	$('.nut3').click(function(event) {
+		$('.noidung').addClass('noidunghienra3');
+		$('.momo').addClass('momohienra');
+	});
+	$('.nut4').click(function(event) {
+		$('.noidung').addClass('noidunghienra4');
+		$('.momo').addClass('momohienra');
+	});
+	$('.nut5').click(function(event) {
+		$('.noidung').addClass('noidunghienra5');
+		$('.momo').addClass('momohienra');
+	});
+	$('.nut7').click(function(event) {
+		$('.noidung').addClass('noidunghienra7');
+		$('.momo').addClass('momohienra');
+	});
+	$('.nut8').click(function(event) {
+		$('.noidung').addClass('noidunghienra8');
+		$('.momo').addClass('momohienra');
+	});
+	$('.nut9').click(function(event) {
+		$('.noidung').addClass('noidunghienra9');
+		$('.momo').addClass('momohienra');
+	});
+	$('.nut10').click(function(event) {
+		$('.noidung').addClass('noidunghienra10');
+		$('.momo').addClass('momohienra');
+	});
+	$('.nut11').click(function(event) {
+		$('.noidung').addClass('noidunghienra11');$('.bonoidung').addClass('noidunghienra11');
+		$('.momo').addClass('momohienra');
+	});
+	$('.nut12').click(function(event) {
+		$('.noidung').addClass('noidunghienra12');
+		$('.momo').addClass('momohienra');
+	});
+	$('.nut13').click(function(event) {
+		$('.noidung').addClass('noidunghienra13');
+		$('.momo').addClass('momohienra');
+	});
+	$('.nut14').click(function(event) {
+		$('.noidung').addClass('noidunghienra14');
+		$('.momo').addClass('momohienra');
+	});
+	$('.nut15').click(function(event) {
+		$('.noidung').addClass('noidunghienra15');
+		$('.momo').addClass('momohienra');
+		$('.main-content').addClass('blur');
+	});
+	$('.nut16').click(function(event) {
+		$('.noidung').addClass('noidunghienra16');
+		$('.momo').addClass('momohienra');
+		$('.main-content').addClass('letmein');
+	});
+	$('.nut17').click(function(event) {
+		$('.noidung').addClass('noidunghienra17');
+		$('.momo').addClass('momohienra');
+		$('.main-content').addClass('letmein2');
+	});
+	$('.nut18').click(function(event) {
+		$('.noidung').addClass('noidunghienra18');
+		$('.momo').addClass('momohienra');
+		$('.main-content').addClass('letmein1');
+	});
+	$('.nut19').click(function(event) {
+		$('.menu').addClass('menuhienra');
+		$('.momo').addClass('momohienra');
+		$('.main-content').addClass('new');
+	});
+	$('.close').click(function(event) {
+		$('.bonoidung').removeClass('noidunghienra11');
+		$('.main-content').removeClass('blur');
+		$('.main-content').removeClass('letmein');
+		$('.main-content').removeClass('letmein1');
+		$('.main-content').removeClass('letmein2');
+		$('.noidung').removeClass('noidunghienra0');
+		$('.noidung').removeClass('noidunghienra1');
+		$('.noidung').removeClass('noidunghienra2');
+		$('.noidung').removeClass('noidunghienra3');
+		$('.noidung').removeClass('noidunghienra4');
+		$('.noidung').removeClass('noidunghienra5');
+		$('.noidung').removeClass('noidunghienra7');
+		$('.noidung').removeClass('noidunghienra8');
+		$('.noidung').removeClass('noidunghienra9');
+		$('.noidung').removeClass('noidunghienra10');
+		$('.noidung').removeClass('noidunghienra11');
+		$('.noidung').removeClass('noidunghienra12');
+		$('.noidung').removeClass('noidunghienra13');
+		$('.noidung').removeClass('noidunghienra14');
+		$('.noidung').removeClass('noidunghienra15');
+		$('.noidung').removeClass('noidunghienra16');
+		$('.noidung').removeClass('noidunghienra17');
+		$('.noidung').removeClass('noidunghienra18');
+		$('.momo').removeClass('momohienra');
+	});
+	$('.momo').click(function(event) {
+		$('.main-content').removeClass('new');
+		$('.menu').removeClass('menuhienra');
+		$('.bonoidung').removeClass('noidunghienra11');
+		$('.main-content').removeClass('blur');
+		$('.main-content').removeClass('letmein');
+		$('.main-content').removeClass('letmein1');
+		$('.main-content').removeClass('letmein2');
+		$('.noidung').removeClass('noidunghienra0');
+		$('.noidung').removeClass('noidunghienra1');
+		$('.noidung').removeClass('noidunghienra2');
+		$('.noidung').removeClass('noidunghienra3');
+		$('.noidung').removeClass('noidunghienra4');
+		$('.noidung').removeClass('noidunghienra5');
+		$('.noidung').removeClass('noidunghienra7');
+		$('.noidung').removeClass('noidunghienra8');
+		$('.noidung').removeClass('noidunghienra9');
+		$('.noidung').removeClass('noidunghienra10');
+		$('.noidung').removeClass('noidunghienra11');
+		$('.noidung').removeClass('noidunghienra12');
+		$('.noidung').removeClass('noidunghienra13');
+		$('.noidung').removeClass('noidunghienra14');
+		$('.noidung').removeClass('noidunghienra15');
+		$('.noidung').removeClass('noidunghienra16');
+		$('.noidung').removeClass('noidunghienra17');
+		$('.noidung').removeClass('noidunghienra18');
+		$('.momo').removeClass('momohienra');
+	});
+});
